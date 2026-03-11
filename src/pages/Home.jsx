9ff3006817a2973ecf9790c1541da85e14393efd
cjgonzales72 <cjgonzales72@aol.com>
2026-03-11 15:32:05 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -214,6 +214,7 @@ export default function Home() {
           if (!open) { setEditingFirm(null); setPreselectedType(null); }
         }}
         onSubmit={handleSubmit}
+        onDelete={setDeletingFirm}
         editingFirm={editingFirm}
         preselectedType={preselectedType}
         existingFirms={firms}
