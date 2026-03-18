a8f1c3c9ea6bfc2745cbf85d4f3b76aa6fd874f5
cjgonzales72 <cjgonzales72@aol.com>
2026-03-18 14:44:27 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,6 +44,7 @@ export default function Home() {
   const [preselectedFirmId, setPreselectedFirmId] = useState(null);
   const [returnToProduct, setReturnToProduct] = useState(false); // track if firm was opened from product
   const [returnToContact, setReturnToContact] = useState(false); // track if product was opened from contact
+  const [returnToContactFromFirm, setReturnToContactFromFirm] = useState(false); // track if firm was opened from contact
 
   const queryClient = useQueryClient();
 
