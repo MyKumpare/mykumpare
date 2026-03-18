02a54a57188c0a3818cd9f020538d29f060babf6
cjgonzales72 <cjgonzales72@aol.com>
2026-03-18 17:24:19 +0000

    File changes

--- src/components/contacts/AddContactDialog.jsx
+++ src/components/contacts/AddContactDialog.jsx
@@ -198,6 +198,7 @@ export default function AddContactDialog({ open, onOpenChange, editingContact, c
       contact_status: contactStatus,
       contact_role: contactRole,
       contact_type: contactType,
+      contact_roles: contactRoles,
       contact_firm_roles: contactFirmRoles,
       gender,
       ethnicity,
