b87bc1c7b8aac9d03c041d337f2b92f31258475c
cjgonzales72 <cjgonzales72@aol.com>
2026-03-19 03:09:26 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -223,6 +223,7 @@ export default function Home() {
   const totalFirms = firms.length;
   const totalProducts = products.length;
   const totalContacts = contacts.length;
+  const totalPortfolios = portfolios.length;
   const hasResults = Object.keys(groupedFirms).length > 0;
 
   return (
